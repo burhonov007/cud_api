7f956ecd3d744b3f05b3061fd84ce574582f3ced
Akmal <akmalburhonov5515@gmail.com>
2023-03-27 20:12:41 +0300

    first commit

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+
+//use Validator;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function index()
+    {
+        $post = Post::all();
+        return response()->json([
+            "success" => true,
+            "message" => "Posts List",
+            "data" => $post
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function store(Request $request)
+    {
+        $input = $request->all();
+        $validator = Validator::make($input, [
+            'title' => 'required|string',
+            'description' => 'required|string',
+            'image' => 'required|string'
+        ]);
+        if ($validator->fails()) {
+            return $this->sendError('Validation Error.', $validator->errors());
+        }
+        $post = Post::create($input);
+        return response()->json([
+            "success" => true,
+            "message" => "Post created successfully.",
+            "data" => $post
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param int $id
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function show($id)
+    {
+        $post = Post::find($id);
+        if (is_null($post)) {
+            return response()->json(['error' => 'Post not found']);
+        }
+        return response()->json([
+            "success" => true,
+            "message" => "Post retrieved successfully.",
+            "data" => $post
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param int $id
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function update(Request $request, Post $post)
+    {
+        $input = $request->all();
+        $validator = Validator::make($input, [
+            'title' => 'required|string',
+            'description' => 'required|string',
+            'image' => 'required|string'
+        ]);
+        if ($validator->fails()) {
+            return $this->sendError('Validation Error.', $validator->errors());
+        }
+        $post->title = $input['title'];
+        $post->description = $input['description'];
+        $post->image = $input['image'];
+        $post->save();
+        return response()->json([
+            "success" => true,
+            "message" => "Post updated successfully.",
+            "data" => $post
+        ]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param int $id
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function destroy(Post $post)
+    {
+        if (is_null($post)) {
+            return response()->json(['error' => 'Post not found ']);
+        }
+        $post->delete();
+        return response()->json([
+            "success" => true,
+            "message" => "Post deleted successfully.",
+            "deleted post data" => $post
+        ]);
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+Route::group(['middleware' => 'api','prefix' => 'auth'], function ($router) {
+    Route::post('/login', 'AuthController@login');
+    Route::post('/register', 'AuthController@register');
+    Route::post('/logout', 'AuthController@logout');
+    Route::post('/refresh', 'AuthController@refresh');
+    Route::get('/profile', 'AuthController@profile');
+
+    Route::get('/posts', 'PostController@index');
+    Route::post('/posts', 'PostController@store');
+    Route::get('/posts/{post}', 'PostController@show');
+
+        Route::group(['middleware'=>'admin'],function () {
+            Route::get('/posts/{post}/edit', 'PostController@edit');
+            Route::delete('/posts/{post}', 'PostController@destroy');
+        });
+});
+
+
+
+
+
+
+
+
